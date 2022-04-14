defd4fdfd5dd9a0c371dfd7e8d30b575d0031781
陈乾 <qianpou@163.com>
2022-04-14 22:25:55 +0800

    添加主模块

---
+++ tlv8-main/src/main/java/com/tlv8/Tlv8Application.java
@@ -0,0 +1,13 @@
+package com.tlv8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tlv8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tlv8Application.class, args);
+	}
+
+}
