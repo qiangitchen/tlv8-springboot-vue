76b7b9cd943652b42a848540cb232e3ebf7d00a1
陈乾 <qianpou@163.com>
2022-04-15 18:42:07 +0800

    启动

---
+++ tlv8-main/src/main/java/com/tlv8/TLv8Application.java
@@ -0,0 +1,11 @@
+package com.tlv8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TLv8Application {
+	public static void main(String[] args) {
+		SpringApplication.run(TLv8Application.class, args);
+	}
+}
