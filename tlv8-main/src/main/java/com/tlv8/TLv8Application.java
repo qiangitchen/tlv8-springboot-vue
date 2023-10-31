220c18c54dce711f47590864a070d244d21def54
陈乾 <qianpou@163.com>
2023-10-31 21:42:53 +0800

    sa-token app log

--- tlv8-main/src/main/java/com/tlv8/TLv8Application.java
+++ tlv8-main/src/main/java/com/tlv8/TLv8Application.java
@@ -1,5 +1,8 @@
 package com.tlv8;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -15,8 +18,10 @@ import cn.dev33.satoken.SaManager;
 @SpringBootApplication
 @ImportResource("classpath:ureport-console-context.xml")
 public class TLv8Application {
+	private static final Logger logger = LoggerFactory.getLogger(TLv8Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(TLv8Application.class, args);
-		System.out.println("启动成功：Sa-Token配置如下：" + SaManager.getConfig());
+		logger.info("启动成功：Sa-Token配置如下：" + SaManager.getConfig());
 	}
 }
