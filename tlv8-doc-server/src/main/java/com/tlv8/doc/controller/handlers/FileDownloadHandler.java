d9a0590b558e7d60277da2b6fcdc8487e66ee582
陈乾 <qianpou@163.com>
2023-11-20 20:57:35 +0800

    文件下载问题

--- tlv8-doc-server/src/main/java/com/tlv8/doc/controller/handlers/FileDownloadHandler.java
+++ tlv8-doc-server/src/main/java/com/tlv8/doc/controller/handlers/FileDownloadHandler.java
@@ -3,19 +3,17 @@ package com.tlv8.doc.controller.handlers;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.tlv8.common.action.ActionSupport;
 import com.tlv8.doc.controller.utils.MimeUtils;
 import com.tlv8.doc.core.io.FileDownloader;
 import com.tlv8.doc.generator.pojo.DocDocPath;
@@ -25,16 +23,16 @@ import com.tlv8.doc.generator.service.DocDocumentService;
 
 @Controller
 @RequestMapping("/DocServer/repository")
-public class FileDownloadHandler extends ActionSupport {
+public class FileDownloadHandler {
 	@Autowired
 	DocDocPathService docDocPathService;
 	@Autowired
 	DocDocumentService docDocumentService;
 
+	@ResponseBody
 	@RequestMapping("/file/download/{fileID}/**")
-	public ResponseEntity<byte[]> handleRequest(HttpServletRequest paramHttpServletRequest,
-			HttpServletResponse paramHttpServletResponse, @PathVariable("fileID") String fileID,
-			@RequestHeader("User-Agent") String userAgent) throws Exception {
+	public void handleRequest(HttpServletRequest paramHttpServletRequest, HttpServletResponse paramHttpServletResponse,
+			@PathVariable("fileID") String fileID, @RequestHeader("User-Agent") String userAgent) throws Exception {
 		DocDocPath dpath = docDocPathService.getDocDocPathByFileID(fileID);
 		DocDocument doc = docDocumentService.getDocumentByDocID(fileID);
 		// 文件创建时间
@@ -56,20 +54,40 @@ public class FileDownloadHandler extends ActionSupport {
 			// 文本类型
 			paramHttpServletResponse.setCharacterEncoding("gb2312");
 		}
-		// 1.设置文件ContentType类型，这样设置，会自动判断下载文件类型
-		paramHttpServletResponse.setContentType(DocType);
-		String formFileName = doc.getFDocName();
-		// 针对IE或者以IE为内核的浏览器：
-		if (userAgent != null && (userAgent.contains("MSIE") || userAgent.contains("Trident"))) {
-			formFileName = java.net.URLEncoder.encode(formFileName, "UTF-8");
-		} else {
-			// 非IE浏览器的处理：
-			formFileName = new String(formFileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+		try {
+			// 1.设置文件ContentType类型，这样设置，会自动判断下载文件类型
+			paramHttpServletResponse.setContentType(DocType);
+			String formFileName = doc.getFDocName();
+			// 针对IE或者以IE为内核的浏览器：
+			if (userAgent != null && (userAgent.contains("MSIE") || userAgent.contains("Trident"))) {
+				formFileName = java.net.URLEncoder.encode(formFileName, "UTF-8");
+			} else {
+				// 非IE浏览器的处理：
+				formFileName = new String(formFileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+			}
+			// 2.设置文件头：最后一个参数是设置下载文件名
+			paramHttpServletResponse.setHeader("Content-Disposition", "attachment; filename=" + formFileName);
+			// 3.通过response获取ServletOutputStream对象(out)
+			ServletOutputStream out = paramHttpServletResponse.getOutputStream();
+			byte[] arrayOfByte = new byte[2048];
+			try {
+				int i;
+				while ((i = inputStream.read(arrayOfByte)) != -1)
+					out.write(arrayOfByte, 0, i);
+			} catch (Exception we) {
+			} finally {
+				try {
+					out.close(); // 关闭输出流
+				} catch (Exception e) {
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				inputStream.close(); // 关闭输入流
+			}
 		}
-		// 2.设置文件头：最后一个参数是设置下载文件名
-		paramHttpServletResponse.setHeader("Content-Disposition", "attachment; filename=" + formFileName);
-		return getByteResponseEntity(inputStream, MediaType.asMediaType(MimeType.valueOf(DocType)), userAgent,
-				formFileName);
 	}
 
 }
